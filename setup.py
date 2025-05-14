67b29fbbf7692164065d136dae69f728260e257b
Juliano Pasa <julianodemellopasa@gmail.com>
2025-05-14 18:39:04 -0300

    Fixed path issue in project package to look only for package files

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version="0.1",
     packages=find_packages(where='src'),
     package_dir={"": 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]
+    py_modules=[splitext(basename(path))[0] for path in glob('src/buspkg/*.py')]
 )
